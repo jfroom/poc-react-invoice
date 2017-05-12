37fd822b38a09f8a33fc8a245fdc99d0982d0ed6
Jeffrey Froom <jeff@jfmk.co>
2017-05-11 20:57:41 -0700

    add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import initialState from './initialState'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
-let store = createStore(rootReducer, initialState)
+let store = createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 render(
   <Provider store={store}>
